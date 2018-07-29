dad555502245ee87acadce168dd45a8bdbb74522
Jack M <devbuild2018@gmail.com>
2018-07-28 19:21:56 -0500

    "Incoporates third party modules and some submodules"

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,8 +2,17 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-var user = os.userInfo();
 
-fs.appendFileSync('greetings.txt', 'Hello ${user.username}!');
+//console.log(_.isString(true));
+//console.log(_.isString('Jaqueline'));
+var filteredArray = _.uniq(['Lacrimos', 1,'Lacrimos', 1, 5, 8, 7, 7, 9])
+console.log(filteredArray);
+
+//console.log(notes.add(9.-2));
+
+//var user = os.userInfo();
+
+//fs.appendFileSync('greetings.txt', 'Hello ${user.username}!');
