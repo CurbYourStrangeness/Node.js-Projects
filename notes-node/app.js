88180b2ca27f75ba4c18ad84b4961ee3851afa11
Jack M <devbuild2018@gmail.com>
2018-07-29 19:23:20 -0500

    "Updates for the json files"

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,15 +1,38 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
+//const os = require('os');
 const _ = require('lodash');
+const yargs = require('yargs');
+
 const notes = require('./notes.js');
+
+const argv = yargs.argv;
+var commd = argv._[0];
+console.log('Command: ', commd);
+console.log('Yargs: ', argv);
+
+
+
+if (commd === 'add'){
+  notes.addNote(argv.title, argv.body);
+}else if (commd === 'list'){
+  notes.getAll();
+}else if (commd === 'read'){
+  notes.getNote(argv.title);
+}else if (commd === 'remove'){
+   notes.removeIt(argv.title);
+}else{
+  console.log('Command not recognized');
+};
+
+
 
 
 //console.log(_.isString(true));
 //console.log(_.isString('Jaqueline'));
-var filteredArray = _.uniq(['Lacrimos', 1,'Lacrimos', 1, 5, 8, 7, 7, 9])
-console.log(filteredArray);
+//var filteredArray = _.uniq(['Lady'])
+//console.log(filteredArray);
 
 //console.log(notes.add(9.-2));
 
