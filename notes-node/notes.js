2d185e7e5bed9979802705b333d2219c254415e2
Jack M <devbuild2018@gmail.com>
2018-08-02 15:29:34 -0500

    "Added Functionality to the 'Add' Note portion of the notes"

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,7 +1,36 @@
 console.log('Starting notes.js');
 
+const files = require('fs');
+
+
+var fetchNotes = () =>{
+
+};
+
+var saveNotes = (notes) => {
+
+};
+
 var addNote = (title, body) => {
- console.log('Adding note', title, body);
+ var notes = [];
+ var note = {
+   title,
+   body
+ };
+
+try{
+  var noteString = files.readFileSync('notes-data.json');
+  notes = JSON.parse(noteString);
+}catch(e){
+
+}
+
+var duplicateNotes = notes.filter((note) => note.title === title);
+
+if(duplicateNotes.length === 0){
+  notes.push(note);
+  files.writeFileSync('notes-data.json', JSON.stringify(notes));
+}
 };
 
 var getAll = () => {
